3c1a087982eeec9a75ec1aa1d61fd7f584db7932
rennanvoa2 <rennanvoa2@zipmail.com.br>
2019-10-15 18:53:50 +0100

    Group meeting changes

--- Programming_For_DS_Project.py
+++ Programming_For_DS_Project.py
@@ -1,5 +1,4 @@
 
-
 #Imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,15 +7,16 @@ import numpy as np
 #Define the URI from the github files (Number of Arrivels and Income)
 income_uri = 'https://raw.githubusercontent.com/rennanvoa2/Programming_DS_Project/master/Income.csv?token=AGBCKJR4XZIDLPKEMDYI5UK5VXNQW'
 arrival_uri = 'https://raw.githubusercontent.com/rennanvoa2/Programming_DS_Project/master/International%20Arrivals.csv?token=AGBCKJWXYPAEJDA36VRCGV25VXLRI'
+metadata_uri = 'https://raw.githubusercontent.com/rennanvoa2/Programming_DS_Project/master/Metadata_Country.csv?token=AGBCKJSRZS3NU57NV5N7MZS5V4P7M'
 
 #Read the Arrivals CSV
 arrivals=pd.read_csv(arrival_uri, header=2)
 
-#Select the columns with usefull data
-arrivals=arrivals[['Country Name', 'Country Code', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']]
+#Select the columns with usefull data we need 2007 here to calculate the grow from 2007 to 2008
+arrivals=arrivals[['Country Name', 'Country Code','2007' , '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']]
 
 #Load metadata CSV
-metadata_country=pd.read_csv('Metadata_Country.csv')
+metadata_country=pd.read_csv(metadata_uri, header=0)
 
 #Merge Arrivals CSV with Metadata CSV
 arrivals_df=arrivals.merge(metadata_country, on='Country Code', how='left')
@@ -38,14 +38,15 @@ arrivals_df = arrivals_df[arrivals_df.Is_Country != False]
 
 #Count number of itens in the DataFrame
 arrivals_df['Country Code'].count()
+
 #Count not nulls in column 2008
-arrivals_df['2008'].count()
+arrivals_df['2007'].count()
 
 #count nulls in 2008
-arrivals_df['2008'].isnull().sum()
+arrivals_df['2007'].isnull().sum()
 
 #drop rows with more than 5 NANs values
-arrivals_df = arrivals_df.dropna(thresh=(len(arrivals_df.columns) - 5))
+arrivals_df = arrivals_df.dropna(thresh=(len(arrivals_df.loc[:,'2008':'2017'].columns) - 5))
 
 #Check if the number of itens is correspondent after drop the rows
 arrivals_df['Country Code'].count()
@@ -54,11 +55,11 @@ arrivals_df['Country Code'].count()
 arrivals_df.drop('Is_Country', inplace=True, axis=1)
 
 
+
 #fill the NaN values with the row mean
-df2 = arrivals_df.loc[:, '2008':'2017']
-df2 = df2.apply(lambda row: row.fillna(row.mean()), axis=1)
-for i in df2.columns:
-    arrivals_df[i] = df2[i]
+arrivals_df.loc[:, '2007':'2017'] = arrivals_df.loc[:, '2007':'2017'].apply(lambda
+               row: row.fillna(row.mean()), axis=1)
+
 
 
 #create feature Avarage in Last 10 Years
@@ -66,9 +67,20 @@ arrivals_df['Avg_Arrivals_10_Years'] = arrivals_df.loc[:,'2008':'2017'].mean(axi
 
 
 
+annual_arrival_growth = pd.DataFrame(index=arrivals_df.index.values)
+
+#Fill the growth of each year in annual_arrival_growth dataframe
+for i in arrivals_df.loc[:,'2008':'2017'].columns:
+    annual_arrival_growth[i] = (arrivals_df[i] - arrivals_df[str(int(i)-1)]) / arrivals_df[str(int(i)-1)]
+   
+
+
 #Create the feature Growth in 10 years
 arrivals_df['Growth10ys']=(arrivals_df['2017']/arrivals_df['2008']-1)
 
+
+#New Growth metric, becouse the last one wasnt good.
+arrivals_df["AVG_Growth"] =  annual_arrival_growth.mean(axis=1)
 
 
 #_____________________________________________________________________________
@@ -77,11 +89,9 @@ arrivals_df['Growth10ys']=(arrivals_df['2017']/arrivals_df['2008']-1)
 #Load income CSV
 income=pd.read_csv(income_uri, header=2)
 
-#Select the columns with usefull data
-income=income[['Country Name', 'Country Code','2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']]
-
-#load Metadata CSV
-metadata_country=pd.read_csv('Metadata_Country.csv')
+#Select the columns with usefull data we need 2007 here to calculate the grow from 2007 to 2008
+income=income[['Country Name', 'Country Code','2007','2008', '2009', '2010', '2011',
+               '2012', '2013', '2014', '2015', '2016', '2017']]
 
 #Merge income CSV and Metadata CSV
 income_df=income.merge(metadata_country, on='Country Code', how='left')
@@ -111,7 +121,7 @@ income_df['2008'].count()
 income_df['2008'].isnull().sum()
 
 #drop rows with more than 5 NANs values
-income_df = income_df.dropna(thresh=(len(income_df.columns) - 5))
+income_df = income_df.dropna(thresh=(len(income_df.loc[:,'2008':'2017'].columns) - 5))
 
 #Check if the number of itens is correspondent after drop the rows
 income_df['Country Code'].count()
@@ -121,97 +131,120 @@ income_df.drop('Is_Country', inplace=True, axis=1)
 
 
 #fill the NaN values with the row mean
-df2 = income_df.loc[:, '2008':'2017']
-df2 = df2.apply(lambda row: row.fillna(row.mean()), axis=1)
-for i in df2.columns:
-    income_df[i] = df2[i]
+income_df.loc[:, '2007':'2017'] = income_df.loc[:, '2007':'2017'].apply(lambda row: row.fillna(row.mean()), axis=1)
+
 
 
 #divided every value for 1000000. Now the scale is in milions of dolars
-for i in range(2008,2018):
-    income_df[str(i)] = income_df[str(i)].apply(lambda x: x / 1000000)
+#for i in range(2008,2018):
+#    income_df[str(i)] = income_df[str(i)].apply(lambda x: x / 1000000)
+#not using anymore
 
 
 #create feature Avarage in Last 10 Years
 income_df['Avg_Income_10_Years'] = income_df.loc[:,'2008':'2017'].mean(axis=1)
 
 
+
 #Create the feature Growth in 10 years
 income_df['Growth10ys']=(income_df['2017']/income_df['2008']-1)
 
 
-total_number_weight = 1
-growth_weight = 1
+annual_income_growth = pd.DataFrame(index=income_df.index.values)
+
+#Fill annual_income_growth
+for i in income_df.loc[:,'2008':'2017'].columns:
+    annual_income_growth[i] = (income_df[i] - income_df[str(int(i)-1)]) / income_df[str(int(i)-1)]  #for i in arrivals_df.loc[:,'2008':'2017'].columns]))
+
+
+
+#New Growth metric, becouse the last one wasnt good.
+income_df["AVG_Growth"] =  annual_income_growth.mean(axis=1)
+
+
+#Create Avarage expenditure per person
+income_df['AVG_expenditure_per_person'] = income_df['Avg_Income_10_Years'] / arrivals_df['Avg_Arrivals_10_Years']
+
+
+
+
+
+#Weigh's for metrics
+arrivals_total_number_weight = 1
+arrivals_growth_weight = 1
+income_total_number_weight = 1
+income_growth_weight = 1
+avg_per_person_weight = 1
 
 #____________________________________________________________
     
     
 #sort by the best avarage arrivals in the last 10 years
-better_arrivals = arrivals_df.sort_values('Avg_Arrivals_10_Years', ascending =False)
+arrivals_df = arrivals_df.sort_values('Avg_Arrivals_10_Years', ascending =False)
 
 
 #divide each value of Growth in 10 years for the sum of the column
-better_arrivals['% growth'] = better_arrivals['Growth10ys'] / better_arrivals['Growth10ys'].sum()
+arrivals_df['% growth'] = arrivals_df['AVG_Growth'] / arrivals_df['AVG_Growth'].sum()
 
-better_arrivals['% growth'].sum()
+arrivals_df['% growth'].sum()
 
 #divide each value of Avarage in 10 years for the sum of the column
-better_arrivals['% avarage'] = better_arrivals['Avg_Arrivals_10_Years'] / better_arrivals['Avg_Arrivals_10_Years'].sum()
+arrivals_df['%Avg_Arrivals'] = arrivals_df['Avg_Arrivals_10_Years'] / arrivals_df['Avg_Arrivals_10_Years'].sum()
 
 
 #Calculate the avarage between Growth and Avarage Numbers of Arrivals
-better_arrivals['Growth x Avarage'] = (growth_weight * better_arrivals['% growth'] +
-               (total_number_weight * better_arrivals['% avarage'])) / (total_number_weight + growth_weight)
+arrivals_df['Growth x Avarage'] = (arrivals_growth_weight * arrivals_df['% growth'] +
+               (arrivals_total_number_weight * arrivals_df['%Avg_Arrivals'])) / (arrivals_total_number_weight + arrivals_growth_weight)
 
 #create a dataframe sorted by Growth X Avarage
-Arrivals_in_growth_vs_arrivals = better_arrivals.sort_values('Growth x Avarage', ascending=False)
+Arrivals_in_growth_vs_arrivals = arrivals_df.sort_values('Growth x Avarage', ascending=False)
 
 #____________________________________________________________--
 
 
 #sort by the best avarage income in the last 10 years
-better_income = income_df.sort_values('Avg_Income_10_Years', ascending =False)
-
-#get the best 40 countries
-#better_income = better_income.iloc[0:40,:]
+income_df = income_df.sort_values('Avg_Income_10_Years', ascending =False)
 
 #divide each value of Growth in 10 years for the sum of the column
-better_income['% growth'] = better_income['Growth10ys'] / better_income['Growth10ys'].sum()
+income_df['% growth'] = income_df['AVG_Growth'] / income_df['AVG_Growth'].sum()
 
-better_income['% growth'].sum()
 
 #divide each value of Avarage in 10 years for the sum of the column
-better_income['% avarage'] = better_income['Avg_Income_10_Years'] / better_income['Avg_Income_10_Years'].sum()
+income_df['%Avg_Incomes'] = income_df['Avg_Income_10_Years'] / income_df['Avg_Income_10_Years'].sum()
 
+#divide each value of Avarage per person for the sum of the column
+income_df['%Avg_Per_Person'] = income_df['AVG_expenditure_per_person'] / income_df['AVG_expenditure_per_person'].sum()
 
 #Calculate the avarage between Growth and Avarage Numbers of Arrivals
-better_income['Growth x Avarage'] = (growth_weight * better_income['% growth'] +
-               (total_number_weight* better_income['% avarage'])) / (total_number_weight + growth_weight)
+income_df['Growth x Avarage x Avg Exp'] = (income_growth_weight * income_df['% growth'] +
+               (income_total_number_weight* income_df['%Avg_Incomes']) + 
+               avg_per_person_weight * income_df['%Avg_Per_Person']) / (income_total_number_weight + 
+                                                income_growth_weight + avg_per_person_weight)
 
 
 #create a dataframe sorted by Growth X Avarage
-income_in_growth_vs_income = better_income.sort_values('Growth x Avarage', ascending=False)
+income_in_growth_vs_income = income_df.sort_values('Growth x Avarage x Avg Exp', ascending=False)
 
 #Drop Belarus, its an outlier in Arrivals Dataset
 Arrivals_in_growth_vs_arrivals = Arrivals_in_growth_vs_arrivals.drop(['Belarus'])
 
-#Drop Myanmar, its an outlier in income Dataset
-income_in_growth_vs_income = income_in_growth_vs_income.drop(['Myanmar'])
+#Drop Congo, its an outlier in income Dataset
+income_in_growth_vs_income = income_in_growth_vs_income.drop(['Congo, Dem. Rep.'])
 
 #________________________________________________________
 
 #get the best 10 results in Arrivals
-arrivals_plot = Arrivals_in_growth_vs_arrivals.iloc[0:10,:]
+arrival_top_10 = Arrivals_in_growth_vs_arrivals.iloc[0:10,:]
 
 #get the best 10 results in income
-income_plot = income_in_growth_vs_income.iloc[0:10,:]
+income_top_10 = income_in_growth_vs_income.iloc[0:10,:]
 
 
 # plotting the points 
-plt.bar(arrivals_plot.index.values, arrivals_plot['Growth x Avarage'], color='blue')
+plt.bar(arrival_top_10.index.values, arrival_top_10['Growth x Avarage'], color='blue')
 
 #Resize the figure to 50x20
-plt.rcParams['figure.figsize'] = (50,20)
+plt.rcParams['figure.figsize'] = (30,10)
 
  
 # naming the x axis 
@@ -233,10 +266,10 @@ plt.show()
 #________________________________________________________
 
 # plotting the points 
-plt.bar(income_plot.index.values, income_plot['Growth x Avarage'], color='orange')
+plt.bar(income_top_10.index.values, income_top_10['Growth x Avarage x Avg Exp'], color='orange')
 
 #Resize the figure to 50x20
-plt.rcParams['figure.figsize'] = (50,20)
+plt.rcParams['figure.figsize'] = (30,10)
 
 # naming the x axis 
 plt.xlabel('Countries') 
@@ -256,14 +289,14 @@ plt.show()
 
 #________________________________________________________
 
-#Create a list with unique countries in arrivals_plot + income_plot
-label_array = list(set(list(arrivals_plot.index.values)+ list(income_plot.index.values)))
+#Create a list with unique countries in arrival_top_10 + income_top_10
+label_array = list(set(list(arrival_top_10.index.values)+ list(income_top_10.index.values)))
 
 
 #Create a dataframe with the sum of top arrivals + top income
 Arrivals_plus_income = pd.DataFrame(index=label_array)
-Arrivals_plus_income['Arrivals GxA'] = better_arrivals['Growth x Avarage']
-Arrivals_plus_income['Income GxA'] = better_income['Growth x Avarage']
+Arrivals_plus_income['Arrivals GxA'] = arrivals_df['Growth x Avarage']
+Arrivals_plus_income['Income GxA'] = income_df['Growth x Avarage x Avg Exp']
 
 #sort the DataFrame by income
 Arrivals_plus_income = Arrivals_plus_income.sort_values('Income GxA', ascending =False)
@@ -279,7 +312,7 @@ plt.bar(Arrivals_plus_income.index.values, Arrivals_plus_income['Arrivals GxA'],
         label='Arrival', bottom=Arrivals_plus_income['Income GxA'], color='blue')
 
 #Resize the figure to 100x20
-plt.rcParams['figure.figsize'] = (100,20)
+plt.rcParams['figure.figsize'] = (30,10)
 
 # naming the x axis 
 plt.xlabel('Top Countries') 
@@ -301,3 +334,6 @@ plt.savefig('Arrivals x Income.png')
 plt.show() 
     
 #_______________________
+
+
+
